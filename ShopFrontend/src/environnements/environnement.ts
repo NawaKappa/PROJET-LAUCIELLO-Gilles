f693c65dd32decc0c32344b5cfa4c438c5147e61
NawaKappa <gilles.lauciello@live.fr>
2021-01-13 16:55:01 +0100

    push project

---
+++ ShopFrontend/src/environnements/environnement.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  backendProducts: 'https://shop-back-gilles-lauciello.herokuapp.com/api/product/',
+  backendClient: 'https://shop-back-gilles-lauciello.herokuapp.com/api/client/'
+};
